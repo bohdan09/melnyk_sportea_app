244690b2e71ae76a367bb2bfdaa795485b8dfdf4
Bohdan Melnyk <bohdan_melnyk@epam.com>
2021-10-02 22:11:35 +0300

    was created statistics repository

---
+++ app/src/main/java/com/example/melnyk_sportea_app/repository/StatisticsRepository.kt
@@ -0,0 +1,21 @@
+package com.example.melnyk_sportea_app.repository
+
+import com.example.melnyk_sportea_app.data.source.local.LocalDataSourceImpl
+import com.example.melnyk_sportea_app.model.Statistics
+
+class StatisticsRepository(
+    private val localDataSourceImpl: LocalDataSourceImpl
+) {
+
+    suspend fun addStatisticsRecord(statisticsRecord: Statistics) {
+        localDataSourceImpl.addStatisticsRecord(statisticsRecord)
+    }
+
+    suspend fun clearAllStatistics() {
+        localDataSourceImpl.clearAllStatistics()
+    }
+
+    fun getAllStatisticsRecords(): List<Statistics> =
+        localDataSourceImpl.getAllStatisticsRecords()
+
+}
